7a737b9879c51eaaa5a7d5e6a7f1d31d2a3ea824
CodeSam621 <101965114+CodeSam621@users.noreply.github.com>
2022-03-20 17:57:44 +1100

    Initial commit
    Initial commit

---
+++ CustomerApi/CustomerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomerApi
+{
+    public class CustomerService
+    {
+        public string GetCustomerName()
+        {
+            return "John";
+        }
+
+        protected string GetCustomerAddress()
+        {
+            return "10, Main Street, Melbourne VIC";
+        }
+
+        private DateTime GetCustomerBirthday()
+        {
+            return DateTime.Now.AddYears(-20) ;
+        }
+
+        private string GetCustomerGreeting(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
